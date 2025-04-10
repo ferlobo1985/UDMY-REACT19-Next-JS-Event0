3f64bb0bcadd57da894ef98e870bc3bab0549dca
Coding rev <fernando.lobo.prez@gmail.com>
2025-04-10 14:02:21 -0300

    32-Home event cards part 2
    32-Home event cards part 2

--- src/components/events_masonry.js
+++ src/components/events_masonry.js
@@ -20,6 +20,13 @@ const breakpointColumnsObj = {
 
 
 export default function EventsMasonryComponent({eventShows,loadMore}){
+    const [events,setEvents] = useState(eventShows)
+
+    const randHeight = (number) => {
+        if(number % 2 === 0) { return 200 }
+        return 300;
+    }
+
 
     return(
        <div className='max-w-5xl mx-auto mt-4 p-5'>
@@ -31,8 +38,35 @@ export default function EventsMasonryComponent({eventShows,loadMore}){
             className='my-masonry-grid'
             columnClassName="my-masonry-grid_column"
         >
+            {events?.map((event,index)=>(
+                <Card 
+                    key={`${event._id}+${index}`}
+                    isFooterBlurred
+                    className='w-full min-h-200 col-span-12 sm:col-span-7'
+                >
+                    <CardHeader className='absolute z-10 top-0 flex-col items-start bg-black/40'>
+                        <p className='text-tiny text-white/60 uppercase font-bold'>{event.venue.name}</p>
+                        <h4 className='text-white/90 font-medium text-xl'>
+                            {event.artist}
+                        </h4>
+                    </CardHeader>
+                    <UIimage
+                        isZoomed={true}
+                        isBlurred={true}
+                        removeWrapper
+                        alt='Card background'
+                        className='z-0 w-full h-full object-cover'
+                        src={`https://picsum.photos/200/${randHeight(index)}?${index}`}
+                    />
+                    <CardFooter className='absolute bg-black/40 bottom-0 z-10'>
+                        <Button radius='full' size='sm' as={Link} href={`/events/${event.slug}`}>
+                            Go to event
+                        </Button>
+                    </CardFooter>
+                    
+                </Card>
 
-
+            ))}
         </Masonry>
 
        </div>
